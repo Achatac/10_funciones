930d6dafb6a030fa5829491567dc143fd4859023
Achatac <achatac@unjbg.edu.pe>
2024-06-24 17:06:37 -0500

    avance de la funcion raices e implementacion de la funcion discriminante

--- ejercicio04.cpp
+++ ejercicio04.cpp
@@ -6,11 +6,25 @@ using namespace std;
 discriminante (int a, int b, int c);
 raices (int a, int b, int a);
 
+discriminante (int a, int b, int c){
+    float d = pow(b,2) - 4 * a * c;
+    return d;
+}
+
 raices (int a, int b, int c){
-    discriminante (int a, int b, int c){
-        float d = pow(b,2) - 4 * a * c
-        if (d )
+    float raiz1, raiz2;
+    if (discriminante(a,b,c) > 0){
+        raiz1 = (-b + sqrt(discriminante(a,b,c)))/2 * a;
+        raiz2 = (-b - sqrt(discriminante(a,b,c)))/2 * a;
+        cout << "Las raices de la ecuacion son: "<< endl;
     }
+    else if (discriminante(a,b,c) == 0){
+
+    }
+    else {
+
+    }
+    
 }
 
 
