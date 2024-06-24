3fa8cb826b07cfcada5dc0302f18c43879a7f54c
Achatac <achatac@unjbg.edu.pe>
2024-06-24 16:46:51 -0500

    declaracion de las funciones

--- ejercicio04.cpp
+++ ejercicio04.cpp
@@ -1,1 +1,11 @@
-Calculo de raices de una ecuacion cuadratica 
+//Calculo de raices de una ecuacion cuadratica 
+#include <iostream>
+using namespace std;
+
+discriminante (int a, int b, int c);
+raices (int d, int b, int a);
+
+
+int main (){
+    return 0;
+}
