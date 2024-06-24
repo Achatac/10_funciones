1bda2a557094f5daa787af35ade18829d94845d7
Achatac <achatac@unjbg.edu.pe>
2024-06-24 16:41:01 -0500

    implementacion del programa principal y correcciones en la funcion hipotenusa

--- ejercicio03.cpp
+++ ejercicio03.cpp
@@ -2,10 +2,16 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 float hipotenusa (float a, float b);
-return sqrt (pow(a,2) + pow(b,2);)
+
+float hipotenusa (float a, float b){
+    return sqrt(pow(a,2) + pow(b,2));
+}
 
 int main (){
+    float a, b;
+    cout << "ingrese el valor del primer cateto: "; cin >> a;
+    cout << "ingrese el valor del segundo cateto: "; cin >> b;
+    cout << "el valor de la hipotenusa es: " << hipotenusa (a,b);
     return 0;
 }
