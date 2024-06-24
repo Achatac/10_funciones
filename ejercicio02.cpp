d9ea849c216af8d960d178400c32afc8d9c86ad4
Achatac <achatac@unjbg.edu.pe>
2024-06-24 16:20:16 -0500

    se agrego for para mostrar todos los numeros aleatorios generados

--- ejercicio02.cpp
+++ ejercicio02.cpp
@@ -4,9 +4,14 @@
 #include <ctime> 
 using namespace std;
 int main (){
-    int cantidadDatos, maximo;
+    int cantidadDatos, maximo, numerosA;
     cout << "ingrese el valor maximo de los numeros aleatorios: "; cin >> maximo;
     cout << "ingrese las cantidad de numeros aleatorios: "; cin >> cantidadDatos;
     srand(static_cast<unsigned>(time(0)));
-    return 0;
+    cout << "Los numeros aleatorios generados son: " << endl;
+    for (int i = 1; i <= cantidadDatos; i++){
+        numersoA = rand() % (maximo + 1);
+        cout << numeroAleatorio << endl;
+    }
+        return 0;
 }
