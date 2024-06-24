a5fbf7ea3fc66cc283ab00e6965e8e7d7def2aea
Achatac <achatac@unjbg.edu.pe>
2024-06-24 15:31:06 -0500

    se anadio cmath

--- ejercicio01.cpp
+++ ejercicio01.cpp
@@ -1,8 +1,11 @@
 //Calculo del area de un circulo 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 float area (float radio);
+float area (float radio){
+}
 
 int main (){
     return 0;
