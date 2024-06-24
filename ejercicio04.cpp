5a350121fa2f468efc24c10801a018774e24e9f6
Achatac <achatac@unjbg.edu.pe>
2024-06-24 16:55:47 -0500

    avance de las funciones e implementacion de cmath

--- ejercicio04.cpp
+++ ejercicio04.cpp
@@ -1,9 +1,18 @@
 //Calculo de raices de una ecuacion cuadratica 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 discriminante (int a, int b, int c);
-raices (int d, int b, int a);
+raices (int a, int b, int a);
+
+raices (int a, int b, int c){
+    discriminante (int a, int b, int c){
+        float d = pow(b,2) - 4 * a * c
+        if (d )
+    }
+}
+
 
 
 int main (){
