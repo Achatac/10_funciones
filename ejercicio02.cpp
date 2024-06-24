ea6735175b85b7d00c861c7f369da3775562dfc7
Achatac <achatac@unjbg.edu.pe>
2024-06-24 16:15:34 -0500

    se anadio la funcion para los numeros aleatorios

--- ejercicio02.cpp
+++ ejercicio02.cpp
@@ -4,5 +4,9 @@
 #include <ctime> 
 using namespace std;
 int main (){
+    int cantidadDatos, maximo;
+    cout << "ingrese el valor maximo de los numeros aleatorios: "; cin >> maximo;
+    cout << "ingrese las cantidad de numeros aleatorios: "; cin >> cantidadDatos;
+    srand(static_cast<unsigned>(time(0)));
     return 0;
 }
