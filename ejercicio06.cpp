756f7d8e680dca4b0565599e17f7bb50b6ed41e6
Achatac <achatac@unjbg.edu.pe>
2024-06-30 14:17:38 -0500

    nombramos a las funciones a usar y corroboarar la implementacion de la libreria string

--- ejercicio06.cpp
+++ ejercicio06.cpp
@@ -4,6 +4,13 @@
 #include <ctime>
 #include <string>
 using namespace std;
+
+int JugadaDeUsuario ();
+int JugadaDeComputadora ();
+int DeterminacionDelGanador (int Jusuario, int Jcomputadora);
+void MostrarJugadas (int jugada);
+
+
 int main (){
     return 0;
 }
