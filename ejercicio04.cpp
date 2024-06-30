b663f75f64c099dba9b9afaeb7e1158cf329ee43
Achatac <achatac@unjbg.edu.pe>
2024-06-30 14:03:07 -0500

    implentamos la funcion main\

--- ejercicio04.cpp
+++ ejercicio04.cpp
@@ -16,20 +16,30 @@ void raices (int a, int b, int c){
     if (discriminante(a,b,c) > 0){
         raiz1 = (-b + sqrt(discriminante(a,b,c)))/(2 * a);
         raiz2 = (-b - sqrt(discriminante(a,b,c)))/(2 * a);
-        cout << "Las raices de la ecuacion son diferentes: ";
+        cout << "Las raices de la ecuacion son diferentes: " << endl;
         cout << raiz1 << endl << raiz2 << endl;
     }
     else if (discriminante(a,b,c) == 0){
         float raizI = (-b)/(2 * a);
-        cout << "Las raices de la ecuacion son iguales: ";
+        cout << "Las raices de la ecuacion son iguales: " << endl;
         cout << raizI << endl;
     }
     else {
-        cout << "las raices de la ecuacion son numeros complejos." ;
+        cout << "las raices de la ecuacion son numeros complejos." << endl;
     }
 }
 
 
 int main (){
+    float a , b , c;
+    cout << "ingrese el valor de la incognita a: "; cin > a;
+    cout << "ingrese el valor de la incognita b: "; cin >> b;
+    cout << "ingrese el valor de la incognita c: "; cin >> c;
+
+    if (a != 0){
+        raices (a,b,c);
+    } else {
+        cout << "la incognita a debe de ser diferente de 0.";
+    }
     return 0;
 }
