b588356fbcef72573be16e9a67745fbbde752adf
Achatac <achatac@unjbg.edu.pe>
2024-06-30 14:00:10 -0500

    completamos la funcion de la discriminate

--- ejercicio04.cpp
+++ ejercicio04.cpp
@@ -6,6 +6,11 @@ using namespace std;
 float discriminante (int a, int b, int c);
 void raices (int a, int b, int c);
 
+float discriminante (int a, int b, c){
+    float d = pow(b,2) - 4 * a * c;
+    return d;
+}
+
 int main (){
     return 0;
 }
