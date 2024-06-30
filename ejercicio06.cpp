a204e47fcba68e412810b724acdfc5e82fde8aab
Achatac <achatac@unjbg.edu.pe>
2024-06-30 14:19:03 -0500

    creamos el arreglo jugadas y cramos su constante

--- ejercicio06.cpp
+++ ejercicio06.cpp
@@ -10,6 +10,10 @@ int JugadaDeComputadora ();
 int DeterminacionDelGanador (int Jusuario, int Jcomputadora);
 void MostrarJugadas (int jugada);
 
+const string jugadas[] = {"Piedra" , "Papel" , "Tijera"};
+int const PIEDRA = 0;
+int const PAPEL = 1;
+int const TIJERA = 2;
 
 int main (){
     return 0;
